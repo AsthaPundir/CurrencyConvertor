6f76dfd98b5cf948c15e2a6937d1f332f5a907a2
Aasthaaa! <pund0006@algonquinlive.com>
2025-01-21 00:36:04 -0500

    Add files via upload

---
+++ CurrencyConverter-main/app/src/main/java/com/college/converter/MainActivity.java
@@ -0,0 +1,52 @@
+package com.college.converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.college.converter.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    private static final Float CONVERSION_RATE = 0.80F;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("MainActivity", "Enter onCreate()");
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Use ViewBinding for the button click listener
+        binding.convertButton.setOnClickListener(this::convertCurrency);
+
+        Log.i("MainActivity", "Exit onCreate()");
+    }
+
+    public void convertCurrency(View view) {
+        Log.i("MainActivity", "Enter convertCurrency() ");
+
+        EditText inputView = binding.entryId;
+        String inputAmount = inputView.getText().toString();
+
+        TextView resultView = binding.resultId;
+
+        if (!inputAmount.isEmpty()) {
+            Float inputAmountDecimal = Float.valueOf(inputAmount);
+            Float resultFloat = inputAmountDecimal * CONVERSION_RATE;
+
+            // Use string resource for "Euros"
+            String resultString = getString(R.string.result_format, resultFloat);
+            resultView.setText(resultString);
+        }
+
+        Log.i("MainActivity", "Exit convertCurrency() ");
+    }
+}
